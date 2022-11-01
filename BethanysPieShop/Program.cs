4a435f26a4dbf5063debb7745088da655cef9a76
Aleksandar <alexandar1993@hotmail.com>
2022-11-01 09:47:58 +0100

    Initial commit

---
+++ BethanysPieShop/Program.cs
@@ -0,0 +1,67 @@
+using BethanysPieShop.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Identity;
+
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("BethanysPieShopDbContextConnection") ?? 
+    throw new InvalidOperationException("Connection string 'BethanysPieShopDbContextConnection' not found.");
+
+builder.Services.AddControllersWithViews()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+    }); ;
+
+//builder.Services.AddScoped<ICategoryRepository, MockCategoryRepository>();
+//builder.Services.AddScoped<IPieRepository, MockPieRepository>();
+
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<IPieRepository, PieRepository>();
+builder.Services.AddScoped<IOrderRepository, OrderRepository>();
+
+
+builder.Services.AddScoped<IShoppingCart, ShoppingCart>(sp => ShoppingCart.GetCart(sp));
+builder.Services.AddSession();
+builder.Services.AddHttpContextAccessor();
+
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+
+builder.Services.AddDbContext<BethanysPieShopDbContext>(options =>
+{
+    options.UseSqlServer(
+        builder.Configuration["ConnectionStrings:BethanysPieShopDbContextConnection"]);
+});
+
+builder.Services.AddDefaultIdentity<IdentityUser>()
+    .AddEntityFrameworkStores<BethanysPieShopDbContext>();
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+app.UseSession();
+app.UseAuthentication();
+app.UseAuthorization();
+
+//app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+
+//app.MapDefaultControllerRoute();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapRazorPages();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/app/{*catchall}", "/App/Index");
+
+
+DbInitializer.Seed(app);
+app.Run();
---
+++ dbo.Pies.sql
@@ -0,0 +1,21 @@
+﻿CREATE TABLE [dbo].[Pies] (
+    [PieId]             INT             IDENTITY (1, 1) NOT NULL,
+    [Name]              NVARCHAR (MAX)  NOT NULL,
+    [ShortDescription]  NVARCHAR (MAX)  NULL,
+    [LongDescription]   NVARCHAR (MAX)  NULL,
+    [AllergyInformation] NVARCHAR (MAX)  NULL,
+    [Price]             DECIMAL (18, 2) NOT NULL,
+    [ImageUrl]          NVARCHAR (MAX)  NULL,
+    [ImageThumbnailUrl] NVARCHAR (MAX)  NULL,
+    [IsPieOfTheWeek]    BIT             NOT NULL,
+    [InStock]           BIT             NOT NULL,
+    [CategoryId]        INT             NOT NULL,
+    CONSTRAINT [PK_Pies] PRIMARY KEY CLUSTERED ([PieId] ASC),
+    CONSTRAINT [FK_Pies_Categories_CategoryId] FOREIGN KEY ([CategoryId]) REFERENCES [dbo].[Categories] ([CategoryId]) ON DELETE CASCADE
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_Pies_CategoryId]
+    ON [dbo].[Pies]([CategoryId] ASC);
+
